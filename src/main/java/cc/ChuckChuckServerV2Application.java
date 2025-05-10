273ec415b6deaffb29fd84928daf137400499a7a
roa7717 <roa7717@kcert.co.kr>
2025-05-10 10:47:34 +0900

    project init

---
+++ src/main/java/cc/ChuckChuckServerV2Application.java
@@ -0,0 +1,13 @@
+package cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckChuckServerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckChuckServerV2Application.class, args);
+    }
+
+}
